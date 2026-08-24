f2f4b45bb7a9f3b3b4c34f3f363427acd266d437
nbri-ged <nbroproject@gmail.com>
2026-08-25 00:25:04 +0530

    Add files via upload

--- js/config/app-config.js
+++ js/config/app-config.js
@@ -4,7 +4,7 @@
    ============================================================ */
 
 // Software Version & Release
-const APP_VERSION = "2.5.6";
+const APP_VERSION = "2.5.7";
 const LOCAL_BOREHOLES_CSV = "CEP 3  Rambukkana-Galagedara - BoreholesDetails (2).csv";
 
 // Data Source Endpoints (Google Sheets Published CSV Feeds)
